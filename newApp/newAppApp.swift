7a8c2354933570628f1e96132b0e93a98eaef215
Yasmin <Yyasmin.1425@gmail.com>
2025-10-20 17:14:05 +0300

    Initial Commit

---
+++ newApp/newAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAppApp.swift
+//  newApp
+//
+//  Created by Jasmin Alshwayhi on 28/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct newAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
